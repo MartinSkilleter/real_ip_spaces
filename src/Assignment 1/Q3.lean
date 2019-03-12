103ca525925b1fbae2e879d4b601dcd22110cea0
Martin Skilleter <u6679334@anu.edu.au>
2019-03-12 15:33:46 +1100

    Finished Question 3

--- src/Assignment 1/Q3.lean
+++ src/Assignment 1/Q3.lean
@@ -4,22 +4,58 @@ namespace hidden
 
 def len {α : Type} : list α -> ℕ
  | [] := 0
- | (x :: L) := 1 + len L
+ | (hd :: tl) := 1 + len tl
 
 def concat {α : Type} : list (list α) -> list α
  | [] := []
  | ([] :: L) := concat L
  | ((a :: l) :: L) := a :: (concat (l :: L))
 
-def nonempty {α : Type}: list α -> bool
+def nonempty {α : Type} : list α -> bool
  | [] := false
  | (_ :: _) := true
+
+lemma nonempty_has_head {α : Type} (L : list α) : nonempty L → ∃ (hd : α), ∃ (tl : list α), hd :: tl = L :=
+begin
+    intros h,
+    have nonempty_def : ∃ (hd : α), ∃ (tl : list α), hd :: tl = L, begin
+        cases L,
+        { have h₂ : nonempty [] = false, by rw nonempty,
+          simp [absurd],
+          have h : false, begin
+            trivial,
+          end,
+        use h,
+        },
+        { use L_hd,
+          use L_tl,
+        },
+    end,
+    apply nonempty_def,
+end
 
 theorem nonempty_concatenation {α : Type} (L : list (list α)) : 
 (nonempty L ∧ ∀ (l ∈ L), nonempty l) → nonempty (concat L) :=
 begin
     intros h,
-    sorry
+    cases h with h_l h_r,
+    {have h₁ : ∃ (a : list α), ∃ (l : list (list α)), a :: l = L, by apply nonempty_has_head L h_l,
+     cases h₁ with hd h₂,
+     cases h₂ with tl eq,
+     have h₃ : hd ∈ L, sorry,
+     have h₄ : ↥(nonempty hd), by apply h_r hd h₃,
+     have h₅ : ∃ (a : α), ∃ (b : list α), a :: b = hd, by apply nonempty_has_head hd h₄,
+     cases h₅ with a b,
+     cases b with b eq,
+
+     
+
+     sorry
+     
+    
+    
+    },
+
 end
 
 end hidden
--- src/Assignment 1/Q5.lean
+++ src/Assignment 1/Q5.lean
@@ -1,8 +1,11 @@
 import logic.basic
 import data.nat.prime
+import data.nat.modeq
+import tactic.norm_num
 
 open nat
 
+-- A lemma to prove that 4 is not prime
 lemma two_divides_four : 2 ∣ 4 :=
 begin
     dsimp [(∣)],
@@ -10,6 +13,7 @@ begin
     refl,
 end
 
+-- A proof that 4 is not prime
 theorem four_is_not_prime : ¬ (prime 4) :=
 begin
     have h : 2 ∣ 4, begin exact two_divides_four end,
@@ -22,21 +26,65 @@ begin
     {cases h₂}
 end
 
-lemma div_leq (a b : ℕ) : a ∣ b → a ≤ b :=
+/-theorem five_is_prime : prime 5 :=
 begin
-sorry
+    simp [prime_def_le_sqrt],
+    split,
+    exact five_geq_two,
+    intros m h w,
+    have h₁ : sqrt 5 = 2, by sorry,
+    simp [h₁] at w,
+    have h₂ : m = 2, begin
+        rw le_antisymm_iff,
+        split,
+        exact w,
+        exact h,
+    end,
+    simp [h₂],
+    assume h₃ : 2 ∣ 5,
+    dsimp [(∣)] at h₃,
+    have h₄ : 5 % 2 = 1 := dec_trivial,
+    have h₅ : ∀ (c : ℕ), 2*c % 2 = 0, begin
+        sorry
+    end,
+    
+
+    sorry
 end
+-/
 
 theorem five_is_prime : prime 5 :=
 begin
-    simp [prime],
-    have h₁ : 5 ≥ 2, begin sorry end,
+    simp [prime_def_lt],
     split,
-    exact h₁,
+    norm_num,
     intros m,
-    have h₂ : m ≤ 5, begin sorry end
-    sorry
+    assume h : m < 5,
+    assume w : m ∣ 5,
+    simp [(∣)] at w,
+    cases w with c,
+    cases h with h,
+    {sorry},
     
+    
+
+    sorry
 end
 
-#print prime
+
+namespace hidden
+
+instance decidable_prime : decidable_pred prime := 
+begin
+    intros n,
+    unfold prime,
+    have left_decidable : decidable (n ≥ 2), begin
+        dsimp [(≥)],
+        apply nat.decidable_le,
+    end,
+    have right_decidable : ∀ (m : ℕ), m ∣ n → m = 1 ∨ m = n, by sorry,
+    sorry
+
+end
+
+end hidden
