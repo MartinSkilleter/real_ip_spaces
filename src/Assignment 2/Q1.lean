f6c106d479cb63b250c0ef5ee63958104ba0bd70
Martin Skilleter <u6679334@anu.edu.au>
2019-03-29 13:14:56 +1100

    Setting up library_search

--- src/Assignment 2/Q1.lean
+++ src/Assignment 2/Q1.lean
@@ -1,5 +1,7 @@
-import logic.basic
-import data.nat.prime
+import data.nat.basic
+import tactic.interactive
+
+def prime (p : ℕ) := p ≥ 2 ∧ ∀ (m < p), m ∣ p → m = 1
 
 open nat
 
@@ -11,17 +13,19 @@ begin
     refl,
 end
 
--- A proof that 4 is not prime
+example {n : ℕ} : n = n := by library_search
+
 theorem four_is_not_prime : ¬ (prime 4) :=
 begin
-    have h : 2 ∣ 4, by exact two_divides_four,
-    simp [prime, not_and_distrib],
+    unfold prime,
+    simp [not_and_distrib],
     right,
-    intros h₁,
-    have h₂ := h₁ 2 h,
-    cases h₂,
-    {cases h₂},
-    {cases h₂}
+    intros h,
+    have h₁ : 2 ∣ 4, by exact two_divides_four,
+    have h₂ : 2 < 4, by exact dec_trivial,
+    have w := h 2 h₂ h₁,
+    have w' : 2 ≠ 1, by exact dec_trivial,
+    contradiction,
 end
 
 lemma five_ge_two : 5 ≥ 2 :=
@@ -40,94 +44,141 @@ begin
     exact h₄,
 end
 
-lemma two_not_div_five : ∀ (n : ℕ), 5 ≠ 2 * n :=
-begin
-    intros n h,
-    cases n,
-    cases h,
-    cases n,
-    cases h,
-    cases n,
-    cases h,
-    cases h,
-end
+meta def case_bash : tactic unit :=
+`[intros h, repeat {cases n, cases h, try {cases h}}]
 
-lemma three_not_div_five : ∀ (n : ℕ), 5 ≠ 3 * n :=
-begin
-    intros n h,
-    cases n,
-    cases h,
-    cases n,
-    cases h,
-    cases h,
-end
+lemma two_not_div_five (n : ℕ) : 5 ≠ 2 * n := by case_bash
 
-lemma four_not_div_five : ∀ (n : ℕ), 5 ≠ 4 * n :=
-begin
-    intros n h,
-    cases n,
-    cases h,
-    cases n,
-    cases h,
-    cases h,
-end
+lemma three_not_div_five (n : ℕ) : 5 ≠ 3 * n := by case_bash
+
+lemma four_not_div_five (n : ℕ) : 5 ≠ 4 * n := by case_bash
 
 theorem five_is_prime : prime 5 :=
 begin
-    simp [prime_def_lt],
-    split,
-    exact five_ge_two,
-    intros m,
-    assume h : m < 5,
-    assume w : m ∣ 5,
-    simp [(∣)] at w,
-    cases w with c w,
-    sorry,
-    
-    
-end
-
-theorem five_is_prime' : prime 5 :=
-begin
-    simp [prime_def_le_sqrt],
+    unfold prime,
     split,
     exact five_ge_two,
     intros m h w,
-    have h₁ : sqrt 5 = 2, by sorry,
-    simp [h₁] at w,
-    have h₂ : m = 2, begin
-        rw le_antisymm_iff,
-        split,
-        exact w,
-        exact h,
-    end,
-    simp [h₂],
-    intros h₃,
-    dsimp [(∣)] at h₃,
-    have h₄ : 5 % 2 = 1 := dec_trivial,
-    have h₅ : ∀ (c : ℕ), 2*c % 2 = 0, begin
-        sorry
-    end,
+    cases m,
+    dsimp [(∣)] at w,
+    cases w with c w,
+    simp only [zero_mul] at w,
+    contradiction,
+    cases m,
+    trivial,
+    cases m,
+    cases w with c w,
+    have w' : 5 ≠ 2 * c, by apply two_not_div_five,
+    contradiction,
+    cases m,
+    cases w with c w,
+    have w' : 5 ≠ 3 * c, by apply three_not_div_five,
+    contradiction,
+    cases m,
+    cases w with c w,
+    have w' : 5 ≠ 4 * c, by apply four_not_div_five,
+    contradiction,
+    cases m,
+    repeat {cases h with h h},
+end
+
+instance decidable_ge {a b : ℕ} : decidable (a ≥ b) :=
+begin
+    dsimp [(≥)],
+    apply nat.decidable_le b a,
+end
+
+instance decidable_prime : decidable_pred prime :=
+begin
+    intros p,
+    unfold prime,
+    -- refine (eq.mpr _ and.decidable),
+    apply_instance,
+end
+
+#print decidable_prime
+
+def prime' (p : ℕ) := p ≥ 2 ∧ ∀ (m ≤ p/2), m ∣ p → m = 1
+
+lemma lt_half_lt {a b : ℕ} : (b > 0) → a ≤ b/2 → a < b :=
+begin
+    intros h w,
+    have h₁ : 2 > 1, by exact dec_trivial,
+    have h₂ : b/2 < b, by apply div_lt_self h h₁,
+    have h₃ : a < b, by apply lt_of_le_of_lt w h₂,
+    exact h₃,
+end
+
+lemma ge_two_gt_zero {p : ℕ} : p ≥ 2 → p > 0 :=
+begin
+    intros h,
+    dsimp [(≥)] at h,
+    have w₁ : 1 < p, by apply lt_of_succ_le h,
+    have w₂ : 0 < 1, by exact dec_trivial,
+    have w₃ : 0 < p, by apply lt_trans w₂ w₁,
+    dsimp [(>)],
+    exact w₃,
+end
+
+lemma div_gt_zero_eq {m p k: ℕ} : m ∣ p → m > 0 → p / m = k → p = m * k :=
+begin
+    intros h₁ h₂ h₃,
+    have h₄ : m * p / m = p := nat.mul_div_cancel_left p h₂,
+    subst h₃,
+    have h₅ : m * p / m = m * (p / m) := nat.mul_div_assoc m h₁,
+    rw h₅ at h₄,
+    exact h₄.symm,
+end
+
+theorem prime_equiv_statement {p : ℕ} : prime p ↔ prime' p :=
+begin
+    split,
+    -- Forwards implication
+    intro h,
+    unfold prime at h,
+    unfold prime',
+    split,
+    exact h.1,
+    have ge := h.1,
+    have h₁ := h.2,
+    intros m w₁ w₂,
+    have w₃ : p > 0, by apply ge_two_gt_zero ge,
+    have w₄ : m < p, by apply lt_half_lt w₃ w₁,
+    have w₅ : m = 1, by apply h₁ m w₄ w₂,
+    exact w₅,
+    -- Converse implication
+    intros w,
+    unfold prime' at w,
+    unfold prime,
+    split,
+    exact w.1,
+    have ge := w.1,
+    intros m w₁ w₂,
+    by_cases (m ≤ p/2),
+    exact w.2 m h w₂,
+    simp at h,
+    have h₁ : p / m ≤ p / 2, sorry,
+    have h₂ : p / m ∣ p, sorry,
+    have h₃ : p / m = 1, by apply w.2 (p/m) h₁ h₂,
+    have h₄ : m > 0, sorry,
+    have h₅ : p = m * 1 := div_gt_zero_eq w₂ h₄ h₃,
+    simp at h₅,
+    subst h₅,
+    have h₆ : p ≥ p, by exact le_refl p,
+    exfalso w₁ h₆,
+
+
+    sorry,
     
 
-    sorry
+    
+
+
 end
 
-
-namespace hidden
-
-instance decidable_prime : decidable_pred prime := 
+instance decidable_prime' : decidable_pred prime' :=
 begin
-    intros n,
-    unfold prime,
-    induction n with n ih,
-    {apply decidable.is_false,
-     intros h,
-     cases h with hl hr,
-     have h₁ : 0 < 2, begin
-        
-     end
-    }
+    intros p,
+    sorry,
 end
 
-end hidden
