70a99cdf6db5ec5434835afd24ae57cbe9747bf7
Martin Skilleter <u6679334@anu.edu.au>
2019-04-06 00:19:54 +1100

    Technically finished Assignment 2

--- src/Assignment 2/Q1.lean
+++ src/Assignment 2/Q1.lean
@@ -12,6 +12,7 @@ begin
     refl,
 end
 
+-- Question 1a
 theorem four_is_not_prime : ¬ (prime 4) :=
 begin
     unfold prime,
@@ -25,6 +26,7 @@ begin
     contradiction,
 end
 
+-- A lemma to prove that 5 is prime; dec_trivial was not used because we can't use tactics in MathLib
 lemma five_ge_two : 5 ≥ 2 :=
 begin
     dsimp [(≥)],
@@ -41,6 +43,8 @@ begin
     exact h₄,
 end
 
+-- This tactic gives a contradiction proof that a non-factor does not divide a number
+-- I couldn't work out how to make the name n be given as an input
 meta def case_bash : tactic unit :=
 `[intros h, repeat {cases n, cases h, try {cases h}}]
 
@@ -50,6 +54,7 @@ lemma three_not_div_five (n : ℕ) : 5 ≠ 3 * n := by case_bash
 
 lemma four_not_div_five (n : ℕ) : 5 ≠ 4 * n := by case_bash
 
+-- Question 1a
 theorem five_is_prime : prime 5 :=
 begin
     unfold prime,
@@ -79,18 +84,27 @@ begin
     repeat {cases h with h h},
 end
 
+-- It is decidable that one natural number is greater than or equal to another
 instance decidable_ge {a b : ℕ} : decidable (a ≥ b) :=
 begin
     dsimp [(≥)],
     apply nat.decidable_le b a,
 end
 
+-- Builds up in stages that the right-hand side of the and statement in the definition of prime
+-- is decidable. It begins by saying that it is decidable if a number equals 1.
+-- Then it is decidable if one number divides another. Finally, a lemma from MathLib is used
+-- which says that it is decidable that, if you have an upper bound for a predicate and that
+-- predicate is decidable for any specific natural number then it is decidable for all natural 
+-- numbers.
 instance decidable_divisors {p : ℕ} : decidable (∀ (m : ℕ), m < p → m ∣ p → m = 1) :=
 begin
     apply nat.decidable_ball_lt p (λ (m : ℕ) (a : m < p), m ∣ p → m = 1),    
 end
 
-set_option pp.implicit true
+-- First instance of decidability about primes. Uses and.decidable and the previous two
+-- lemmas to say that the whole statement is decidable.
+-- Note that haveI is used to update the instance cache after the instances are declared.
 instance decidable_prime : decidable_pred prime :=
 begin
     intros p,
@@ -100,8 +114,12 @@ begin
     apply and.decidable,
 end
 
+-- A second definition of primality. We will prove that this is equivalent to the
+-- first definition.
 def prime' (p : ℕ) := p ≥ 2 ∧ ∀ (m ≤ p/2), m ∣ p → m = 1
 
+-- If a natural number a is less than or equal to b/2 then it is less than b
+-- Used as a lemma in prime_im_prime'
 lemma lt_half_lt {a b : ℕ} : (b > 0) → a ≤ b/2 → a < b :=
 begin
     intros h w,
@@ -111,6 +129,7 @@ begin
     exact h₃,
 end
 
+-- Essentially used to say that if p is prime then p > 0
 lemma ge_two_gt_zero {p : ℕ} : p ≥ 2 → p > 0 :=
 begin
     intros h,
@@ -122,6 +141,7 @@ begin
     exact w₃,
 end
 
+-- Non-zero factors of a number respect multiplication
 lemma div_gt_zero_eq {m p k: ℕ} : m ∣ p → m > 0 → p / m = k → p = m * k :=
 begin
     intros h₁ h₂ h₃,
@@ -132,6 +152,8 @@ begin
     exact h₄.symm,
 end
 
+-- The reciprocals of positive natural numbers behave as you would
+-- expect them to under ≤
 lemma dvd_pos_lt {a b c : ℕ} : c ≤ b → 0 < c → a / b ≤ a / c :=
 begin
     intros h₁ h₂,
@@ -145,6 +167,7 @@ begin
     exact h₁,
 end
 
+-- The first definition prime → the second definition prime'
 theorem prime_im_prime' {p : ℕ} : prime p → prime' p :=
 begin
     intro h,
@@ -161,6 +184,7 @@ begin
     exact w₅,
 end
 
+-- The second definition prime' → the first definition prime
 theorem prime'_im_prime {p : ℕ} : prime' p → prime p :=
 begin
     intros w,
@@ -174,6 +198,8 @@ begin
     exact w.2 m h w₂,
     simp at h,
     have two_gt_zero : 2 > 0, by exact dec_trivial,
+    -- Wasn't written as a lemma because I felt it would be unwieldy to pass
+    -- hypotheses around
     have one_lt_half : p / 2 ≥ 1, begin
         have le : 2 ≤ p := ge,
         have z : 2/2 ≤ p / 2 := nat.div_le_div_right le,
@@ -181,6 +207,7 @@ begin
         rw [h₁] at z,
         exact z,
     end,
+    -- Same as above. Relies on hypotheses.
     have h₄ : m > 0, begin
         have zero_lt_one : 0 < 1, by exact dec_trivial,
         have h₂ : 0 < p / 2, by apply lt_of_lt_of_le zero_lt_one one_lt_half,
@@ -199,12 +226,16 @@ begin
     exact (absurd w₁ h₇),
 end
 
+-- Uses the constructor for ↔ and the above implications to give an equivalence statement
 theorem prime_equiv_statement {p : ℕ} : prime p ↔ prime' p :=
 ⟨prime_im_prime', prime'_im_prime⟩
 
+-- See explanation above decidable_divisors. Construction is the same but upper bound used is p/2
+-- instead of p
 instance decidable_divisors' {p : ℕ}: decidable (∀ (m : ℕ), m < p / 2 → m ∣ p → m = 1) :=
 nat.decidable_ball_lt (p / 2) (λ (m : ℕ) (a : m < p / 2), m ∣ p → m = 1)
 
+-- Says that it is possible to decidable if a natural number is prime under prime'
 instance decidable_prime' : decidable_pred prime' :=
 begin
     intros p,
@@ -214,9 +245,12 @@ begin
     apply and.decidable,
 end
 
+-- Gives a "better" instance for decidable_prime by applying the equivalence of
+-- prime and prime'. It is better because you only need to check half as many natural numbers
+-- for large cases.
 instance decidable_prime_v2 : decidable_pred prime :=
 begin
     intros p,
-    apply decidable_of_iff' _ (@prime_equiv_statement p),
+    apply decidable_of_iff' (prime' p) (@prime_equiv_statement p),
 end
 
