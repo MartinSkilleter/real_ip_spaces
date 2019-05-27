fd14bfeade9a74fa733011a2e87f046a9e0ea214
Martin Skilleter <u6679334@anu.edu.au>
2019-05-27 20:32:11 +1000

    Added references to report. Done for now

--- src/inner_product_spaces/real_ip/ip_normed_space.lean
+++ src/inner_product_spaces/real_ip/ip_normed_space.lean
@@ -256,10 +256,10 @@ begin
     exact w,
 end
 
-lemma norm_leq_of_norm_sq_leq {β : Type*} [normed_space ℝ β] {x : β} {a : ℝ} {k : a ≥ 0}: ∥x∥^2 ≤ a^2 → ∥x∥ ≤ a :=
+lemma norm_sqr_leq_iff_norm_leq {β : Type*} [normed_space ℝ β] {x : β} {a : ℝ} {k : a ≥ 0}: ∥x∥^2 ≤ a^2 ↔ ∥x∥ ≤ a :=
 begin
-    intros h,
     have w := sqrt_le (mul_nonneg (norm_nonneg x) (norm_nonneg x)) (mul_nonneg k k),
-    rw [←pow_two, ←pow_two, sqrt_sqr (norm_nonneg _), sqrt_sqr k] at w,
-    exact w.2 h,
+    repeat {rw [←pow_two] at w},
+    rw [sqrt_sqr (norm_nonneg _), sqrt_sqr k] at w,
+    exact w.symm,
 end
